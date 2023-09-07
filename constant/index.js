4f99e65ae1047d395b5a554d63b1501f9d4b2f79
Shilpi Jain <met2shilpi@gmail.com>
2023-09-07 23:37:07 +0530

    setup layout

---
+++ constant/index.js
@@ -0,0 +1,5 @@
+const TOP_HEADER = `Worldwide free shipping on all orders!`
+
+export {
+    TOP_HEADER
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
-import Footer from "../../components/m-web/footer";
-import Header from "../../components/m-web/header";
+import Copyright from "../../components/layout/copy-right";
+import Footer from "../../components/layout/footer";
+import Header from "../../components/layout/header";
+import Topheader from "../../components/layout/top-header";
 
 export default function Home() {
   return (
     <>
-    <Header/>
-    <div>
-      <img className="banner" src="https://images4.alphacoders.com/130/1307940.png"/>
-    </div>
-    <Footer/>
+      <Topheader/>
+      <Header/>
+        <div>
+          <img className="sliderBanner" src="https://images4.alphacoders.com/130/1307940.png"/>
+        </div>
+      <Footer/>
+      <Copyright/>
     </>
   )
 }
