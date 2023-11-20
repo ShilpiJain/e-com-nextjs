80ebab75bee6dce864177026ceff89bae15d65ff
Shilpi Jain <met2shilpi@gmail.com>
2023-11-20 23:51:01 +0530

    image doamin

--- next.config.js
+++ next.config.js
@@ -1,13 +1,28 @@
 /** @type {import('next').NextConfig} */
-let imageDomains =[
-    "images4.alphacoders.com",
-    "devhero.shop"
-]
+// let imageDomains =[
+//     "images4.alphacoders.com",
+//     "devhero.shop"
+// ]
 
-const nextConfig = {
-    images:{
-        domains: imageDomains
-    }
+// const nextConfig = {
+//     images:{
+//         domains: imageDomains
+//     }
+// }
+
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'images4.alphacoders.com',
+            pathname: '**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'devhero.shop',
+            pathname: '**',
+          }
+      ],
+    },
 }
-
-module.exports = nextConfig
